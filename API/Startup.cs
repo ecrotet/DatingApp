23755c4c1d1d041778d3c74dac00be489277c975
Ecrotet <engie.crotet@eskerpartners.com>
2022-04-11 17:17:08 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -50,10 +52,11 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
